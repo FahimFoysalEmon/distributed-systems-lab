5a5aeb1deeeab10e9ce2c0326c076db4e900882b
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-08-27 11:47:21 +0600

    freq of characters

--- src/main/java/lab/distributedsystems/hashmap/problems/FrequencyOfCharacters.java
+++ src/main/java/lab/distributedsystems/hashmap/problems/FrequencyOfCharacters.java
@@ -7,13 +7,10 @@ import java.util.HashMap;
 public class FrequencyOfCharacters {
 
     public FrequencyOfCharacters(PhoneBook phoneBook, String key){
-        System.out.println("FULL KEY - "+key);
-        System.out.println("FULL VALUE - "+phoneBook.getContact(key));
-
+        System.out.println("KEY - "+key);
 
         HashMap<Character, Integer> frequency = new HashMap<>();
 
-        System.out.println("ONE BY ONE CHARACTERS\n");
         for (int i = 0; i < key.length(); i++) {
 
             char c  = key.charAt(i);
